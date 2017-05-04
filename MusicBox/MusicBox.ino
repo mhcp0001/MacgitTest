c84126b9096550e1941affd958642e1ab184eab9
mhcp0001 <teraoka@ii.is.kit.ac.jp>
2017-05-04 14:48:44 +0900

    Add Button release

--- MusicBox/MusicBox.ino
+++ MusicBox/MusicBox.ino
@@ -10,6 +10,8 @@ const int D_pin = 2;
 const int E_pin = 3;
 const int F_pin = 4;
 const int G_pin = 5;
+unsigned long time_m =0;
+int push = 0;
 
 int C_value = 0;
 int D_value = 0;
@@ -18,7 +20,7 @@ int F_value = 0;
 int G_value = 0;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin( 9600 );
+  Serial.begin(57600);
   // initialize digital pin 8 as an output. 
   pinMode(led_pin, OUTPUT);
 }
@@ -34,27 +36,61 @@ void loop() {
   G_value = analogRead( G_pin );
 
   analogWrite(led_pin, C_value/4 );
-  Serial.println( byte(C_value/4) );
+  //Serial.println( byte(C_value/4) );
+  time_m=millis();
 
   if(C_value<PRESS){
     tone(PINSP,NOTE_C6,BEAT) ;  // ド
+    if(push == 0){
+      Serial.print("NOTE_C pushed:");
+      Serial.println(time_m);
+      push = 1;
+    }
      //delay(BEAT*2) ;
   }else if(D_value<PRESS){
     tone(PINSP,NOTE_D6,BEAT) ;  // レ
+    if(push == 0){
+      Serial.print("NOTE_D pushed:");
+      Serial.println(time_m);
+      push = 1;
+    }
     //delay(BEAT*2) ;
   }else if(E_value<PRESS){
     tone(PINSP,NOTE_E6,BEAT) ;  // ミ
+    if(push == 0){
+      Serial.print("NOTE_E pushed:");
+      Serial.println(time_m);
+      push = 1;
+    }
     //delay(BEAT*2) ;
   }else if(F_value<PRESS){
     tone(PINSP,NOTE_F6,BEAT) ;  // ファ
+    if(push == 0){
+      Serial.print("NOTE_F pushed:");
+      Serial.println(time_m);
+      push = 1;
+    }
     //delay(BEAT*2) ;
   }else if(G_value<PRESS){
     tone(PINSP,NOTE_G6,BEAT) ;  // ソ
+    if(push == 0){
+      Serial.print("NOTE_G pushed:");
+      Serial.println(time_m);
+      push = 1;
+    }
     //delay(BEAT*2) ;
+  }else if(push==1){
+    Serial.print("Button relese:");
+    Serial.println(time_m);
+    push = 0; 
   }
 
   if(mode == 1){
-    
+    game();
   }
 
+}
+
+void game(){
+  
 }
